37d763a484ce60f4bfe8a683c941efe705fd4742
PranavDalve <pranavudalve@gmail.com>
2026-03-07 16:32:23 +0530

    Adding deviceID

--- src/services/routerEvent.service.ts
+++ src/services/routerEvent.service.ts
@@ -2,23 +2,22 @@ import { dataSource } from "../config/database.js";
 
 export const getRouterEvents = async () => {
 
-  const events = await dataSource.query(`
+    const events = await dataSource.query(`
     SELECT
-        e.device_id,
-        e.timestamp,
+        e.device_id AS "deviceId",
+        e."timestamp",
         e.type,
         e.details,
         m.member_code
     FROM router_events e
     LEFT JOIN member_events m
     ON (
-          (e.details->'device_details'->>'mac') = m.mac
-       OR (e.details->'device_details'->>'ip') = m.ip
-       OR (e.details->'domain_activity'->>'source_ip') = m.ip
+        (e.details->'device_details'->>'mac') = m.mac
+    OR (e.details->'device_details'->>'ip') = m.ip
+    OR (e.details->'domain_activity'->>'source_ip') = m.ip
     )
-    ORDER BY e.timestamp DESC
-    LIMIT 200
-  `);
+    ORDER BY e."timestamp" DESC
+    `);
 
   return events;
 };
