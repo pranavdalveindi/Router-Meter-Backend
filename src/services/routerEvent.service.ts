83d4d853e5463d49ae4aa9e57cdbb8b0f88a77a3
PranavDalve <pranavudalve@gmail.com>
2026-03-09 23:36:17 +0530

    Adding member events

--- src/services/routerEvent.service.ts
+++ src/services/routerEvent.service.ts
@@ -4,19 +4,24 @@ export const getRouterEvents = async () => {
 
     const events = await dataSource.query(`
     SELECT
-        e.device_id AS "deviceId",
-        e."timestamp",
-        e.type,
-        e.details,
-        m.member_code
+    e.device_id AS "deviceId",
+    e."timestamp",
+    e.type,
+    e.details,
+    m.member_code,
+    m.hhid,
+    m.device_type
     FROM router_events e
     LEFT JOIN member_events m
     ON (
-        (e.details->'device_details'->>'mac') = m.mac
-    OR (e.details->'device_details'->>'ip') = m.ip
-    OR (e.details->'domain_activity'->>'source_ip') = m.ip
+        e.device_id = m.device_id
+    AND (
+            (e.details->'device_details'->>'mac') = m.mac
+        OR (e.details->'device_details'->>'ip') = m.ip
+        OR (e.details->'domain_activity'->>'source_ip') = m.ip
     )
-    ORDER BY e."timestamp" DESC
+    )
+    ORDER BY e."timestamp" DESC;
     `);
 
     console.log("ROUTER EVENT SERVICE LOADED");
